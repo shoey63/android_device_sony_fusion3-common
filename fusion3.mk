2fee46ebb55c05d05707990273f3a3b19832dfcf
Joe Wooster <jeebobs@btinternet.com>
2014-12-18 13:42:28 +0000

    fusion3: Disable LPA decode
    Change-Id: I64c6dae7f922701706ccd0b20bcc087008676153

--- fusion3.mk
+++ fusion3.mk
@@ -190,8 +190,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.mode=endfire \
     persist.audio.handset.mic=analog \
     persist.audio.lowlatency.rec=false \
-    ro.qc.sdk.audio.fluencetype=none \
-    lpa.decode=true
+    ro.qc.sdk.audio.fluencetype=none
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
