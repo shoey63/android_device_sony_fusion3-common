4678cdaa03498f2ed6d83edf128e6edbc85acc4f
Joe Wooster <jeebobs@btinternet.com>
2015-01-01 12:08:16 +0000

    Revert "fusion3: Disable LPA decode"
    As far as I know, this is still needed for AwesomePlayer.
    
    This reverts commit 2fee46ebb55c05d05707990273f3a3b19832dfcf.
    
    Change-Id: I37fd0b9675c08003aa87da96ae081c2cc9dd9ace

--- fusion3.mk
+++ fusion3.mk
@@ -193,7 +193,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.mode=endfire \
     persist.audio.handset.mic=analog \
     persist.audio.lowlatency.rec=false \
-    ro.qc.sdk.audio.fluencetype=none
+    ro.qc.sdk.audio.fluencetype=none \
+    lpa.decode=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
