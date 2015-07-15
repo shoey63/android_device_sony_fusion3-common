f7407a57fdf8db067dbd029581d81b48d0a1f23d
olivieer <yves.olli@gmail.com>
2015-07-15 13:44:09 +0200

    fusion3: Use a bigger recovery font
    Not necessarily needed for TWRP as it uses minuitwrp (which uses TW_THEME to determine the font size). However, it's required for the healthd which uses the stock minui - the default font is barely readable on our high-res displays.
    
    Change-Id: Ieaf7bb79d6d49de1cc41e77f1dda3c3bad90f2fc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,6 +129,7 @@ BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := device/sony/fusion3-common/custombootimg.mk
 TARGET_RECOVERY_FSTAB := device/sony/fusion3-common/rootdir/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
