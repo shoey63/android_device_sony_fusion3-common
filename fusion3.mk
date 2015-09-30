19de8b9f4da8fc2780e4d9cd11085010a17c8fa2
Humberto Borba <humberos@gmail.com>
2015-09-30 17:28:44 -0300

    fusion3: Use IIO driver interface instead of DASH
    this change is part of:
    https://github.com/CyanogenMod/android_device_sony_fusion3-common/commit/4d6f02e06b7370fbfa4fecd962146f1fda3a5c35
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I6b582b7d17e1d4db7c81bd3597976aa92c7afc47

--- fusion3.mk
+++ fusion3.mk
@@ -171,10 +171,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     lights.qcom
 
-# Sensors
-PRODUCT_PACKAGES += \
-    sensors.msm8960
-
 # Thermal management
 PRODUCT_PACKAGES += \
     thermanager
@@ -227,6 +223,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
+    $(COMMON_PATH)/rootdir/init.sony.rc:root/init.sony.rc \
     $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
     $(COMMON_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
 
